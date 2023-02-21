925b7cc43db1627a4c3f99ab6e5e740d463558ce
NathanGoncalves <nathan@liberaai.com>
2023-02-21 16:00:30 -0300

    Snake with classes

--- main.py
+++ main.py
@@ -1,139 +1,125 @@
 import pygame, random
+from pygame.math import Vector2
 
 pygame.init() 
 
-displayWidth = 800
-displayHeight = 600
-
-gameWindow = pygame.display.set_mode((displayWidth, displayHeight))
 white = (255, 255, 255)
 red = (255, 0, 0)
-blue = (0, 255, 0)
-
-running = True
+green = (0, 255, 0)
+blue = (0, 0, 255)
+bgGreen = (95, 167, 0)
 
 snakeImg = pygame.image.load('assets/Green_Snake_Pixel.png')
 
 clock = pygame.time.Clock()
 
-foodX = random.randint(0, displayWidth)
-foodY = random.randint(0, displayHeight)
+cell_size = 20
+cell_number = 30
 
-snakeX = 0
-snakeY = 0
+gameWindow = pygame.display.set_mode((cell_size * cell_number, cell_size * cell_number))
 
-snakeX_move = 0 
-snakeY_move = 0
+class SNAKE: 
+    def __init__(self):
+        self.body = [Vector2(2, 0), Vector2(1,0), Vector2(0,0)]
+        self.direction =  Vector2(1,0)
 
-def snake(x, y):
-    # gameWindow.blit(snakeImg, (x, y))
-    pygame.draw.rect(gameWindow, blue, [x, y, 20, 20])
+    def drawSnake(self):
+        for block in self.body: 
+            blockRect = pygame.Rect(block.x * cell_size, block.y * cell_size , cell_size, cell_size)
+            pygame.draw.rect(gameWindow, green, blockRect)
+
+    def moveSnake(self):
+        body = self.body[:-1]
+        body.insert(0, body[0] + self.direction)
+        self.body = body[:]
+
+    def addBlock(self): 
+        body = self.body[:]
+        body.insert(0, body[0] + self.direction)
+        self.body = body[:]
 
 
-def handleRightMove():
-    global snakeX_move
-    global snakeY_move
-    snakeX_move = 10
-    snakeY_move = 0
-
-def handleLeftMove():
-    global snakeX_move
-    global snakeY_move
-    snakeX_move = -10
-    snakeY_move = 0
-
-def handleDownMove():
-    global snakeX_move
-    global snakeY_move
-    snakeX_move = 0
-    snakeY_move = 10
-
-def handleUpMove():
-    global snakeX_move
-    global snakeY_move
-    snakeX_move = 0
-    snakeY_move = -10
-
-def handleStopMove(): 
-    global snakeX_move
-    global snakeY_move
-    snakeX_move = 0
-    snakeY_move = 0
-
-def boundaries():
-    global displayHeight
-    global displayWidth
-    global snakeX 
-    global snakeY
+class FRUIT: 
+    def __init__(self):
+        self.randomPosition()
     
-    if snakeX > displayWidth: 
-        snakeX = 0
-    elif snakeX < 0: 
-        snakeX = displayWidth
+    def drawFruit(self):
+        fruitRect = pygame.Rect(self.pos.x * cell_size , self.pos.y * cell_size, cell_size, cell_size)
+        pygame.draw.rect(gameWindow, red, fruitRect)
+
+    def randomPosition(self): 
+        self.x = random.randint(0, cell_number - 1)
+        self.y = random.randint(0, cell_number - 1)
+        self.pos = Vector2(self.x, self.y)
+
+class MAIN:
+    def __init__(self): 
+        self.snake = SNAKE()
+        self.fruit = FRUIT()
     
-    if snakeY > displayHeight:
-        snakeY = 0
-    elif snakeY < 0: 
-        snakeY = displayHeight
+    def update(self): 
+        self.snake.moveSnake()
+        self.checkCollision()
 
-def snakeCollision(): 
-    global snakeX
-    global snakeY
-    global foodX
-    global foodY 
-    # VALOR DE 20 COMO ALTURA DO BLOCO 
-    if snakeX + 20 > foodX  and snakeX < foodX + 20 and snakeY + 20 > foodY and snakeY < foodY + 20:
-        randomFoodCords()
+    def draw(self): 
+        self.snake.drawSnake()
+        self.fruit.drawFruit()
 
-def randomFoodCords():
-    global foodX 
-    global foodY 
-    foodX = random.randint(0, displayWidth)
-    foodY = random.randint(0, displayHeight)
+    def checkCollision(self):
+        self.checkBoundaries()
+        if self.fruit.pos == self.snake.body[0]: 
+            self.fruit.randomPosition()
+            self.snake.addBlock()
 
-def foodSpawn():
-   global foodX 
-   global foodY
-   pygame.draw.rect(gameWindow, red, [foodX, foodY, 20, 20])
+    def checkBoundaries(self): 
+        if not 0 <= self.snake.body[0].x:
+            self.snake.body[0].x = cell_number -1
+        elif not cell_number >= self.snake.body[0].x :
+            self.snake.body[0].x = 0 
+
+        if not 0 <= self.snake.body[0].y:
+            self.snake.body[0].y = cell_number -1
+        elif not cell_number >= self.snake.body[0].y :
+            self.snake.body[0].y = 0 
+
+        for block in self.snake.body[1:]: 
+            if block == self.snake.body[0]:
+                self.gameOver()
+    
+    def gameOver(self):
+        self.snake = SNAKE() 
+        self.fruit = FRUIT()
+
 
 def gameLoop():
-    global running
-    global snakeX
-    global snakeY
+    main = MAIN()
 
+    running = True
+
+    SCREEN_UPDATE = pygame.USEREVENT
+    pygame.time.set_timer(SCREEN_UPDATE, 175)
     while running:
-
         for event in pygame.event.get():
             if event.type == pygame.QUIT: 
                 running = False
-            
-            if event.type == pygame.KEYDOWN:
-                match event.key:
-                    case pygame.K_UP:
-                        handleUpMove()
+            if event.type == SCREEN_UPDATE: 
+                main.update()
+            if event.type == pygame.KEYDOWN: 
+                match event.key: 
+                    case pygame.K_UP: 
+                        main.snake.direction = Vector2(0, -1)
                     case pygame.K_DOWN:
-                        handleDownMove()
-                    case pygame.K_RIGHT:
-                        handleRightMove()
+                        main.snake.direction = Vector2(0, 1)
                     case pygame.K_LEFT:
-                        handleLeftMove()
-                    case pygame.K_SPACE:
-                        handleStopMove()
-    
-
-        snakeX += snakeX_move
-        snakeY += snakeY_move
-
-        boundaries()
-        snakeCollision()
-
-        gameWindow.fill(white)
-        foodSpawn()
-        snake(snakeX, snakeY)
+                        main.snake.direction = Vector2(-1, 0)
+                    case pygame.K_RIGHT:
+                        main.snake.direction = Vector2(1, 0)
         
+        gameWindow.fill(bgGreen)
 
+        main.draw()
         pygame.display.update()
-        clock.tick(10)
+        clock.tick(60)
 
 
 gameLoop()
